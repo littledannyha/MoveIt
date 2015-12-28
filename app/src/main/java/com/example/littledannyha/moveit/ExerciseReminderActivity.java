00d82b67b97dd58b72264c11a48450caa96582b8
Dan Haugh <daniel_haugh@brown.edu>
2015-12-28 06:05:46 -0500

    Fixed caching issue with PendingIntent and removed service because it wasn't necessary

--- app/src/main/java/com/example/littledannyha/moveit/ExerciseReminderActivity.java
+++ app/src/main/java/com/example/littledannyha/moveit/ExerciseReminderActivity.java
@@ -1,35 +1,40 @@
 package com.example.littledannyha.moveit;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class ExerciseReminderActivity extends AppCompatActivity {
+    public static final String EXTRA_COUNTDOWN_DURATION = "com.littledannyha.moveit.extra_countdown_duration";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_reminder);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        int duration = getIntent().getIntExtra(IntervalCountdownService.EXTRA_COUNTDOWN_DURATION, 5);
-
+        int duration = getIntent().getIntExtra(EXTRA_COUNTDOWN_DURATION, 5);
+        clearPendingIntentCache();
         Button restartButton = (Button) findViewById(R.id.restartCountdownButton);
+        restartButton.setText("Another Reminder? (" + duration + "min)");
+
         restartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startedIntent = getIntent();
-                int duration = startedIntent.getIntExtra(IntervalCountdownService.EXTRA_COUNTDOWN_DURATION, 5);
-                startCountdownService(duration);
+                int durationInSeconds = getIntent().getIntExtra(EXTRA_COUNTDOWN_DURATION, 5);
+                Log.d("Duration selected: ", Integer.toString(durationInSeconds));
+                setAlarmManager(durationInSeconds);
                 onBackPressed();
                 returnToMainMenu();
             }
         });
-        restartButton.setText("Another Reminder? (" + duration + "min)");
 
         Button doneButton = (Button) findViewById(R.id.finishCountdownButton);
         doneButton.setOnClickListener(new View.OnClickListener() {
@@ -41,18 +46,30 @@ public class ExerciseReminderActivity extends AppCompatActivity {
         });
     }
 
-    public void startCountdownService(int duration){
-        Intent intent = new Intent(getApplicationContext(), IntervalCountdownService.class);
-        intent.setAction(IntervalCountdownService.ACTION_START_COUNTDOWN);
-        intent.putExtra(IntervalCountdownService.EXTRA_COUNTDOWN_DURATION, duration);
-        startService(intent);
-    }
-
     public void returnToMainMenu(){
         Intent startMain = new Intent(Intent.ACTION_MAIN);
         startMain.addCategory(Intent.CATEGORY_HOME);
         startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(startMain);
+    }
+
+    public void clearPendingIntentCache() {
+        Intent intent = new Intent(this, ExerciseReminderActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(EXTRA_COUNTDOWN_DURATION, 3);
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent alarmIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        alarmIntent.cancel();
+    }
+
+    public void setAlarmManager(int durationInSeconds) {
+        Log.d("Reminder Duration", Integer.toString(durationInSeconds));
+        Intent intent = new Intent(this, ExerciseReminderActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(EXTRA_COUNTDOWN_DURATION, durationInSeconds);
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent alarmIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + durationInSeconds * 1000, alarmIntent);
     }
 
 }
--- app/src/main/java/com/example/littledannyha/moveit/IntervalSelectionActivity.java
+++ app/src/main/java/com/example/littledannyha/moveit/IntervalSelectionActivity.java
@@ -1,5 +1,8 @@
 package com.example.littledannyha.moveit;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -19,29 +22,17 @@ public class IntervalSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interval_selection);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
         Button intervalStartButton = (Button) findViewById(R.id.startIntervalButton);
         intervalStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText durationPicker = ((EditText) findViewById(R.id.durationPicker));
                 Integer duration = Integer.parseInt(durationPicker.getText().toString());
-
-                Log.d("Interval Button", "Duration selected: " + duration.toString());
-                startCountdownService(duration);
-
+                setAlarmManager(duration);
                 returnToMainMenu();
             }
         });
 
-    }
-
-    public void startCountdownService(int duration){
-        Intent intent = new Intent(getApplicationContext(), IntervalCountdownService.class);
-        intent.setAction(IntervalCountdownService.ACTION_START_COUNTDOWN);
-        intent.putExtra(IntervalCountdownService.EXTRA_COUNTDOWN_DURATION, duration);
-        startService(intent);
     }
 
     public void returnToMainMenu(){
@@ -52,24 +43,27 @@ public class IntervalSelectionActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_interval_selection, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void setAlarmManager(int durationInMinutes) {
+        Log.d("Selection Duration", Integer.toString(durationInMinutes));
+        Intent intent = new Intent(this, ExerciseReminderActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(ExerciseReminderActivity.EXTRA_COUNTDOWN_DURATION, durationInMinutes);
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent alarmIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + durationInMinutes * 1000, alarmIntent);
+    }
+
 }
